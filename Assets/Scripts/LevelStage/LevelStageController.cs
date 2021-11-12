f06412f3786fb4c3d107a4384743b962086e203d
rizqyn9 <54632504+rizqyn9@users.noreply.github.com>
2021-11-12 22:33:05 +0700

    fix: add ui level child

--- Assets/Scripts/LevelStage/LevelStageController.cs
+++ Assets/Scripts/LevelStage/LevelStageController.cs
@@ -4,10 +4,41 @@ using UnityEngine;
 
 public class LevelStageController : Singleton<LevelStageController>
 {
+    [Header("Properties")]
+    public GameObject levelStageContainer;
+    public GameObject levelPrefab;
+    public int minShowLevel = 10;
+
+    [Header("Debug")]
+    public int resCount = 0;
+    public List<UI_Level> levels = new List<UI_Level>();
+
     public void init()
     {
-        
-        Debug.LogWarning("Level Stage");
+        resCount = ResourceManager.Instance.ListLevel.Count;
+        int render = resCount > minShowLevel ? resCount : minShowLevel;
+
+        for(int i = 0; i < render; i++)
+        {
+            instanceLevelChild(i);
+        }
+    }
+
+    void instanceLevelChild(int _index)
+    {
+        UI_Level _level = Instantiate(levelPrefab, levelStageContainer.transform).GetComponent<UI_Level>();
+        levels.Add(_level);
+
+        _level.init(
+            _index,
+            getLevelBase(_index)
+        );
+    }
+
+    LevelBase getLevelBase(int _index)
+    {
+        if (_index >= resCount) return null;
+        return ResourceManager.Instance.ListLevel[_index];
     }
 
     public void Btn_Close()
---
+++ Assets/Scripts/LevelStage/UI_Level.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Level : MonoBehaviour
+{
+    [Header("Debug")]
+    public int index;
+    public LevelBase levelBase;
+
+    public void init(int _index, LevelBase _levelBase = null)
+    {
+
+    }
+}
