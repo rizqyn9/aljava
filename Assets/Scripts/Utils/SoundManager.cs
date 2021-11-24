cb7cbe6ed954095648bc72f2f370c50f829252c8
Koga <79469238+agarangga55@users.noreply.github.com>
2021-11-24 13:12:45 +0700

    add : sound singleton var

--- Assets/Scripts/Utils/SoundManager.cs
+++ Assets/Scripts/Utils/SoundManager.cs
@@ -7,6 +7,16 @@ namespace Aljava
 {
     public class SoundManager : Singleton<SoundManager>
     {
-        public AudioClip namaVariable;
+        public AudioClip BGM;
+        public AudioClip ButtonSFX;
+        public AudioClip Star1;
+        public AudioClip Star2;
+        public AudioClip Star3;
+        public AudioClip Win;
+        public AudioClip Lose;
+        public AudioClip RepairSFX;
+        public AudioClip MachineDoneSFX;
+        public AudioClip MachineSoundSFX;
+        public AudioClip MachineOverheatSFX;
     }
 }
